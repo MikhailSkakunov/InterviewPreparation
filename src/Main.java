f8bcde4510eec06ab0533a1e8ae8463d9791a66d
MikhailSkakunov <82500910+MikhailSkakunov@users.noreply.github.com>
2022-08-09 17:25:46 +0300

    Commit_01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Person person = new Person()
+                .setName("Shayla").setLastName("Mayer").setMiddleName("Younger")
+                        .setCountry("Australia").setAddress("Villa, Q6").setPhone("+3768073834838")
+                        .setAge(35).setGender("women");
+
+        System.out.println(person);
+
+        Shape shape = new Triangle();
+        shape.draw();
+
+        Shape shape1 = new Square();
+        shape1.draw();
     }
 }
